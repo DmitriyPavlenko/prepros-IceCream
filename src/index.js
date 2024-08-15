18327c566384e8d96fc9d8cbac54efa8029a8f7f
Dmitriy Pavlenko <pavlenko.d.o@gmail.com>
2024-08-15 20:03:08 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './sass/main.scss';
+import './js/mobile-menu';
+import './js/slider';
+import './js/up';
+import './js/scroll';
+import './js/scroll-animation';
+import './js/modal';
---
+++ src/js/modal.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-modal');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[franchise-modal-open]'),
+    closeModalBtn: document.querySelector('[franchise-modal-close]'),
+    modal: document.querySelector('[franchise-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-modal');
+  }
+})();
